b3384894ab23a67a3ab1299027e0d3cc445e5945
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-01-21 14:21:12 -0300

    Solution

--- db/models.py
+++ db/models.py
@@ -2,13 +2,11 @@ from django.db import models
 
 
 class Actor(models.Model):
-    # (Já existente no seu projeto)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
 
 class Genre(models.Model):
-    # (Já existente no seu projeto)
     name = models.CharField(max_length=255)
 
 
@@ -41,4 +39,7 @@ class MovieSession(models.Model):
     movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f"{self.movie.title} {self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        return (
+            f"{self.movie.title} "
+            f"{self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
