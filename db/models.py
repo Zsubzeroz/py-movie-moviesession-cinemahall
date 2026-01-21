0401f1a0db1c23f8d014e754328bfddf6af2ceae
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-01-21 14:16:54 -0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,16 +1,44 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    # (Já existente no seu projeto)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
 class Genre(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    # (Já existente no seu projeto)
+    name = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(to=Actor)
+    genres = models.ManyToManyField(to=Genre)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
 
     def __str__(self) -> str:
         return self.name
 
 
-class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(to=CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.movie.title} {self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
